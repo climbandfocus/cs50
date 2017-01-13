3bb7c1575473655c5c3d2ca1c3ed73100ad21706
climbandfocus <spencer.vliegen@googlemail.com>
2017-01-12 18:14:01 -0700

    Create initials.c

---
+++ initials.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void){
+    string initials = get_string();
+    // print the first uppercase initial if only one word
+    printf("%c", toupper(initials[0]));
+    // loop to print out the first letter after a space
+    for (int i = 0; i < strlen(initials); i++){
+        if (initials[i] == ' ' && initials[i+1] != '\0'){
+    printf("%c", toupper(initials[i+1]));
+    }
+}
+    printf("\n");
+}
